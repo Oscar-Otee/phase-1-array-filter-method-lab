145a449a70dd24f9ee5b7cf62e72cefda2dda40b
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-06-11 19:40:52 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution here
+
+function findMatching(driverNames, string) {
+    const matchDrivers = [];
+    for (const driver of driverNames) {
+        if (driver === string) {
+            matchDrivers.push(driver)
+        }
+        else if (driver.toLowerCase() === string.toLowerCase()) {
+            matchDrivers.push(driver);
+        }
+        
+        }
+        return matchDrivers;
+    }
+    
+
+
+
+function fuzzyMatch(driverNames, string) {
+    const matchString = [];
+    for (const driver of driverNames) {
+        if (driver.slice(0, 2) === string) {
+            matchString.push(driver);
+        
+        }
+    }
+    return matchString;
+}
+
+
+function matchName(driverNames, string) {
+    const matchDrivers = [];
+    for (const driver of driverNames) {
+        if (driver.name === string) {
+            matchDrivers.push(driver);
+    
+        }
+    }
+    return matchDrivers;
+}
+
